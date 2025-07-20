3e405f290e37aaa1bcadbdd71abcf2801b1da03e
nexussafiret <tubrocafi@gmail.com>
2025-07-20 23:25:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    token: 'MTM5NTQ5NzEwMDI3NTQ4NjgxMA.GV7WFb.UKOKgO0O4Lu9dtWZd5QFICZRZID8fDmNbggBQk', // Ganti dengan token bot Discord Anda
+    channelId: '1395490657560297573',
+    roleId: '1395488699365458154',
+    adminRoleId: '1362872935744012600', // ID role admin yang dapat menggunakan commands
+    logsChannelId: '1395576825912102932', // ID channel untuk logs pembelian
+    ticketCategoryId: '1395658893849526302', // ID category untuk membuat ticket
+    giveawayTicketCategoryId: '1395932542611492945', // ID category untuk tiket giveaway
+    paymentLogsChannelId: '1395576825912102932', // ID channel untuk logs pembayaran (bisa sama dengan logs pembelian)
+    giveawayLogsChannelId: '1395702756911743127', // ID channel untuk logs giveaway
+    reportLogsChannelId: '1396135737677713418', // ID channel untuk logs report
+    garansiChannelId: '1396148555432988813', // ID channel untuk sistem garansi
+    garansiLogsChannelId: '1395576825912102932', // ID channel untuk logs garansi (menggunakan channel logs yang sama)
+    testimoniChannelId: '1396269795648868392', // ID channel untuk testimoni
+    statusChannelId: '1362867241695318250', // ID channel untuk status bot
+    activityLogsChannelId: '1395715844700504084' // ID channel untuk logs aktivitas member
+}; 
